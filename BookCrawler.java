3d55d7550f7206456e8e1239953b99b9b5822c7c
xddShoter <132182713+xddShoter@users.noreply.github.com>
2023-05-06 16:41:22 +0800

    Add files via upload

--- BookCrawler.java
+++ BookCrawler.java
@@ -14,52 +14,60 @@ public class BookCrawler {
 	
 	private static class SearchResult {
         private String title;
-	private String language;
-	private String author;
+		private String language;
+		private String author;
         private String price;
-	private String link;
+		private String link;
         private String image;
 
         public SearchResult(String title,String language,String author,String price, String link, String image) {
-            	this.title = title;
-		this.language=language;
-		this.author = author;
-		this.price = price;
-          	this.link = link;
-            	this.image = image;
+            this.title = title;
+			this.language=language;
+			this.author = author;
+			this.price = price;
+            this.link = link;
+            this.image = image;
         }
     }
 	
-    public static String SearchBooks(String keyword) throws Exception {
+    public static String SearchBooks(String keyword,boolean bool,int howManyBooks) throws Exception {
         String url1 = SEARCH_URL1 + keyword + SEARCH_URL2;//傳入關鍵字形成目標網址
         Document doc1 = Jsoup.connect(url1).userAgent(USER_AGENT).get();//抓下目標網址的資料
-	Elements books = doc1.select(".table-td");//24本書所處的欄位
-	Gson gson = new Gson();
-	SearchResult[] searchResults = new SearchResult[books.size()];
-	int i=0;
-        for (Element book : books) {
-           
- 		String title = book.select("h4").text();
-            	//System.out.println("Title: " + title);//標題
-			
-		String language = book.select("div.type p").first().text();
-           	//System.out.println("Language: " + language);//中文or外文
-			
-		String author = book.select("p.author").text();
-           	// System.out.println("Author: " + author);//作者
-			
-		Element P = book.select("ul.price li").first();
-		String price = P.select("b").last().text();
-            	// System.out.println("Price: " + price);//價錢
-			
-            	Elements link = book.select("a[href]");
-            	//System.out.println("Link: " + link.attr("href"));//購買連結
+		Elements books = doc1.select(".table-td");//本頁書籍所處欄位
+		Gson gson = new Gson();
+		SearchResult[] searchResults = new SearchResult[howManyBooks];
+		int i=0,page=2;
+		while(i<howManyBooks){
+			for (Element book : books) {
+			   
+				String title = book.select("h4").text();
+				System.out.println("Title: " + title);//標題
+				
+				String language = book.select("div.type p").first().text();
+				System.out.println("Language: " + language);//中文or外文
+				
+				String author = book.select("p.author").text();
+				System.out.println("Author: " + author);//作者
+				
+				Element P = book.select("ul.price li").first();
+				String price = P.select("b").last().text();
+				System.out.println("Price: " + price);//價錢
+				
+				Elements link = book.select("a[href]");
+				System.out.println("Link: " + link.attr("href"));//購買連結
 
-          	Elements image = book.select("img[src]");
-            	//System.out.println("Image: " + image.attr("data-src"));//圖片
-			
-		searchResults[i++] = new SearchResult(title,language,author,price,link.attr("href"),image.attr("data-src"));//存入
-        }
+				Elements image = book.select("img[src]");
+				System.out.println("Image: " + image.attr("data-src"));//圖片
+				if(language.equals("中文書") && bool)//true只抓中文
+					searchResults[i++] = new SearchResult(title,language,author,price,link.attr("href"),image.attr("data-src"));//存入
+				else if(language.equals("外文書") && !bool)//false只抓外文
+					searchResults[i++] = new SearchResult(title,language,author,price,link.attr("href"),image.attr("data-src"));
+				if(i==howManyBooks) break;
+			}
+			url1 = "https://search.books.com.tw/search/query/cat/all/sort/1/v/1/spell/3/ms2/ms2_1/page/"+Integer.toString(page++)+"/key/"+keyword;
+			doc1 = Jsoup.connect(url1).userAgent(USER_AGENT).get();//下一頁
+			books = doc1.select(".table-td");
+		}
 		return gson.toJson(searchResults);//回傳Json檔
     }
 }
--- BookCrawlerTest.java
+++ BookCrawlerTest.java
@@ -6,11 +6,10 @@ public class BookCrawlerTest {
 		BookCrawler crawl = new BookCrawler();
 		String keyword = scanner.nextLine();
 		try {
-            		String results = crawl.SearchBooks(keyword);
+            String results = crawl.SearchBooks(keyword,false,30);//(關鍵字,中文true外文false,想抓幾本)
 			System.out.println(results);
-        } 
-		catch (Exception e) {
-           		 System.out.println("Error occurred: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error occurred: " + e.getMessage());
         }
 	}
 }
