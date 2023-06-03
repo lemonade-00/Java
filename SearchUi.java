d67f9ac7a47326a8af3a8e3ef86370abea2b2eeb
alway4open <134674622+alway4open@users.noreply.github.com>
2023-06-03 19:01:42 +0800

    Add files via upload

--- SearchUi.java
+++ SearchUi.java
@@ -9,36 +9,46 @@ import javax.swing.JButton;
 import javax.swing.JOptionPane;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.UIManager;//
+import javax.swing.table.DefaultTableModel;//
+import javax.swing.table.TableCellRenderer;//
+import javax.swing.DefaultCellEditor;//
+import javax.swing.JTable;//
+import javax.swing.JCheckBox;//
+import java.awt.Dimension;//
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Label;
+import java.awt.Component;//
+
 
 public class SearchUi extends JFrame {
 	private JPanel topPanel;//放 搜尋 那行(部分一)
 	private JPanel sequencePanel;//放 排序 那行(部分二)
-	//private JPanel sequencePanel2;//放 排序 第二行(部分三)
-	private JPanel outputPanel;//放 資料(部分四)
+	private JPanel sequencePanel2;//放 排序 第二行(部分三)
+	//private JPanel outputPanel;//放 資料(部分四)
 	private JScrollPane outputScrollPane;//讓outputPanel放
+	private JTable ouyputTable;//放 資料(部分四)
 	
 	private Label searchLabel;
-	private JLabel picLabel;//資料第一排的文字 pictuer
-	private JLabel bookLabel;//資料第一排的文字 book
-	private JLabel libraryLabel;//資料第一排的文字 library
-	private JLabel buttonLabel;//資料第一排的文字 button
+	//private JLabel picLabel;//資料第一排的文字 pictuer
+	//private JLabel bookLabel;//資料第一排的文字 book
+	//private JLabel libraryLabel;//資料第一排的文字 library
+	//private JLabel buttonLabel;//資料第一排的文字 button
 	
 	private ButtonGroup searchOptions;//排序方式的按鈕組
 	private JRadioButton search1;//排序方式一
 	private JRadioButton search2;//排序方式二
 	private JRadioButton search3;//排序方式三
 	
-	/*private ButtonGroup searchOptions2;
+	private ButtonGroup searchOptions2;
 	private JRadioButton button1;
 	private JRadioButton button2;
 	private JRadioButton button3;
-	*/
+	
 	private JTextField searchField;//search柏克萊的文字(搜尋格)
 	
 	private JButton mainButton;//切換按鈕
@@ -51,7 +61,9 @@ public class SearchUi extends JFrame {
 	public SearchUi() {
 		// TODO
 		super("搜尋頁面");//標題
-		setLayout(new GridLayout(4,1));//將介面分成三主要部分(FlowLayout()的排版會比較好看些)
+		FlowLayout layout = new FlowLayout();
+		layout.setHgap(1000);
+		setLayout(layout);//GridLayout(4,1)將介面分成三主要部分(FlowLayout()的排版會比較好看些)
 		
 		loginUi = new LoginUi(this);//創建登入跟帳戶的介面
 		accountUi = new AccountUi(this,loginUi);
@@ -73,14 +85,14 @@ public class SearchUi extends JFrame {
 		searchOptions.add(search2);
 		searchOptions.add(search3);
 		
-		/*button1 = new JRadioButton("button1",true);//選擇排序方式的按鈕
+		button1 = new JRadioButton("button1",true);//選擇排序方式的按鈕
 		button2 = new JRadioButton("button2",false);
 		button3 = new JRadioButton("button3",false);
 		searchOptions = new ButtonGroup();
 		searchOptions.add(button1);
 		searchOptions.add(button2);
 		searchOptions.add(button3);
-		*/
+		
 		topPanel = new JPanel();//部分一
 		topPanel.setLayout(new FlowLayout());
 		topPanel.add(mainButton);
@@ -95,12 +107,13 @@ public class SearchUi extends JFrame {
 		sequencePanel.add(search2);
 		sequencePanel.add(search3);
 		
-		/*sequencePanel2 = new JPanel();//部份三
+		sequencePanel2 = new JPanel();//部份三
 		sequencePanel2.setLayout(new FlowLayout());
 		sequencePanel2.add(button1);
 		sequencePanel2.add(button2);
 		sequencePanel2.add(button3);
-		*/
+		
+		/*
 		outputPanel = new JPanel();//部分四
 		outputPanel.setLayout(new GridLayout(10,4));//**********************數字需要改成(印出資料數+1,4)************
 		picLabel = new JLabel("pictuer");
@@ -137,11 +150,76 @@ public class SearchUi extends JFrame {
             outputPanel.add(button);
 		}
 		outputScrollPane = new JScrollPane(outputPanel);
-		
+		*/
+		Object[][] data={
+		{"Kelly","Female","Button1"},
+		{"Peter","Male","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Amy","Female","Button1"},
+		{"Tony","Male","Button1"}};
+		String[] columns={"column1","column2","Button"};
+		JTable ouyputTable=new JTable(data,columns);
+		ouyputTable.getColumn("Button").setCellRenderer(new ButtonRenderer());
+        ouyputTable.getColumn("Button").setCellEditor(new ButtonEditor(new JCheckBox()));
+		ouyputTable.setPreferredScrollableViewportSize(new Dimension(400,300));
+		outputScrollPane = new JScrollPane(ouyputTable);
 		
 		add(topPanel);//將四個部分放入JFrame
 		add(sequencePanel);
-		//add(sequencePanel2);
+		add(sequencePanel2);
 		add(outputScrollPane);
 		
 		MyEventListner handler = new MyEventListner();//監控 回搜尋頁按鈕、搜尋按鈕、帳戶按鈕、搜尋輸入格
@@ -184,6 +262,77 @@ public class SearchUi extends JFrame {
 				}
 				setVisible(false);//隱藏搜尋頁面
 			}
+		}
+	}
+	
+	class ButtonRenderer extends JButton implements TableCellRenderer {
+
+		public ButtonRenderer() {
+			setOpaque(true);
+		}
+
+		@Override
+		public Component getTableCellRendererComponent(JTable table, Object value,
+				boolean isSelected, boolean hasFocus, int row, int column) {
+			if (isSelected) {
+				setForeground(table.getSelectionForeground());
+				setBackground(table.getSelectionBackground());
+			} else {
+				setForeground(table.getForeground());
+				setBackground(UIManager.getColor("Button.background"));
+			}
+			setText((value == null) ? "" : value.toString());//設定ButtonRenderer名稱
+			return this;
+		}
+	}
+	
+	class ButtonEditor extends DefaultCellEditor {
+
+		protected JButton button;
+		private String label;
+		private boolean isPushed;
+
+		public ButtonEditor(JCheckBox checkBox) {
+			super(checkBox);
+			button = new JButton();
+			button.setOpaque(true);
+			button.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					fireEditingStopped();//他把button要做的事寫在另一個地方，所以要寫這個讓listener做事
+				}
+			});
+		}
+
+		@Override
+		public Component getTableCellEditorComponent(JTable table, Object value,
+				boolean isSelected, int row, int column) {
+			if (isSelected) {
+				button.setForeground(table.getSelectionForeground());
+				button.setBackground(table.getSelectionBackground());
+			} else {
+				button.setForeground(table.getForeground());
+				button.setBackground(table.getBackground());
+			}
+			label = (value == null) ? "" : value.toString();//設定button名稱
+			button.setText(label);
+			isPushed = true;
+			return button;
+		}
+
+		@Override
+		public Object getCellEditorValue() {
+			if (isPushed) {
+				JOptionPane.showMessageDialog(button, label + ": Ouch!");//按鈕做的事
+			}
+			isPushed = false;
+			return label;
+		}
+
+		@Override
+		public boolean stopCellEditing() {
+			isPushed = false;
+			return super.stopCellEditing();
 		}
 	}
 }
